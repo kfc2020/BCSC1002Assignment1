852065c7801508d3ef964fc60d96a2083674ba68
karanfaujdar <karan222224@gmail.com>
2020-08-30 14:30:13 +0530

    create a equal() and hashcode() method for the Library class

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -36,4 +36,17 @@ public class Library {
                 "allTheBooksThatAreCurrentlyAvailable=" + Arrays.toString(allTheBooksThatAreCurrentlyAvailable) +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getAllTheBooksThatAreCurrentlyAvailable(), library.getAllTheBooksThatAreCurrentlyAvailable());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getAllTheBooksThatAreCurrentlyAvailable());
+    }
 }

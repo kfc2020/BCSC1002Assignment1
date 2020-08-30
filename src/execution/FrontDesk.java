fe8516237723e5f0ca3d80ff03ff78709a2824ba
karanfaujdar <karan222224@gmail.com>
2020-08-30 15:56:29 +0530

    create a private static field for the FrontDesk

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -12,10 +12,19 @@ import definitions.Student;
 import java.util.Scanner;
 
 public class FrontDesk {
+
+    private static final int ISSUE_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int SHOW_ALL_ISSUES_BOOK = 3;
+    private static final int INVENTORY_LIST = 4;
+    private static final int EXIT = 5;
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Library library = new Library();
         int clientInput;
         Student student = new Student();
+
     }
 }
